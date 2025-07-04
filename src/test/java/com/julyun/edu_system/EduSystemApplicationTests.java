b540aa319800ad70ac8b18ae5ff7102be6ddbc27
JuRyeon33 <thwnfus0330@gmail.com>
2025-07-04 15:38:58 +0900

    edu-system

---
+++ src/test/java/com/julyun/edu_system/EduSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julyun.edu_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
